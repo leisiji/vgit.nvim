081207260e1403d40c36338df3a1f361163ecb17
leisiji <2265215145@qq.com>
2025-05-02 07:22:19 +0800

    fix: keep git-show behavior as before, first find in index and then commit tree

--- lua/vgit/libgit2/git_show.lua
+++ lua/vgit/libgit2/git_show.lua
@@ -9,8 +9,6 @@ function git_show.lines(reponame, filename, commit_hash)
 
   if not reponame then return nil, { 'reponame is required' } end
 
-  commit_hash = commit_hash or 'HEAD'
-
   local object_ptr
   local commit_ptr
   local commit_tree_ptr
@@ -20,32 +18,45 @@ function git_show.lines(reponame, filename, commit_hash)
   local oid
   local data
   local len
+  local index_ptr
+  local index_entry
 
   local repo_ptr = ffi.new('git_repository*[1]')
   local ret = libgit2.cli.git_repository_open(repo_ptr, reponame)
   if ret ~= 0 then return nil, libgit2.error() end
 
-  object_ptr = ffi.new('git_object*[1]')
-  ret = libgit2.cli.git_revparse_single(object_ptr, repo_ptr[0], commit_hash)
-  if ret ~= 0 then goto OUT end
+  -- find in index at first
+  if commit_hash == nil then
+    index_ptr = ffi.new('git_index*[1]')
+    ret = libgit2.cli.git_repository_index(index_ptr, repo_ptr[0])
+    if ret ~= 0 then goto OUT end
+    index_entry = libgit2.cli.git_index_get_bypath(index_ptr[0], filename, 0)
+  end
 
-  commit_ptr = ffi.new('git_commit*[1]')
-  oid = libgit2.cli.git_object_id(object_ptr[0])
-  ret = libgit2.cli.git_commit_lookup(commit_ptr, repo_ptr[0], oid)
-  if ret ~= 0 then goto OUT end
+  if index_entry then
+    oid = index_entry.id
+  else
+    object_ptr = ffi.new('git_object*[1]')
+    ret = libgit2.cli.git_revparse_single(object_ptr, repo_ptr[0], commit_hash or 'HEAD')
+    if ret ~= 0 then goto OUT end
 
-  commit_tree_ptr = ffi.new('git_tree*[1]')
-  ret = libgit2.cli.git_commit_tree(commit_tree_ptr, commit_ptr[0])
-  if ret ~= 0 then goto OUT end
+    commit_ptr = ffi.new('git_commit*[1]')
+    oid = libgit2.cli.git_object_id(object_ptr[0])
+    ret = libgit2.cli.git_commit_lookup(commit_ptr, repo_ptr[0], oid)
+    if ret ~= 0 then goto OUT end
 
-  entry_ptr = ffi.new('git_tree_entry*[1]')
-  ret = libgit2.cli.git_tree_entry_bypath(entry_ptr, commit_tree_ptr[0], filename)
-  if ret ~= 0 then goto OUT end
+    commit_tree_ptr = ffi.new('git_tree*[1]')
+    ret = libgit2.cli.git_commit_tree(commit_tree_ptr, commit_ptr[0])
+    if ret ~= 0 then goto OUT end
 
-  if libgit2.cli.git_tree_entry_type(entry_ptr[0]) ~= libgit2.cli.GIT_OBJECT_BLOB then goto OUT end
+    entry_ptr = ffi.new('git_tree_entry*[1]')
+    ret = libgit2.cli.git_tree_entry_bypath(entry_ptr, commit_tree_ptr[0], filename)
+    if ret ~= 0 then goto OUT end
+    if libgit2.cli.git_tree_entry_type(entry_ptr[0]) ~= libgit2.cli.GIT_OBJECT_BLOB then goto OUT end
+    oid = libgit2.cli.git_tree_entry_id(entry_ptr[0])
+  end
 
   blob_ptr = ffi.new('git_blob*[1]')
-  oid = libgit2.cli.git_tree_entry_id(entry_ptr[0])
   ret = libgit2.cli.git_blob_lookup(blob_ptr, repo_ptr[0], oid)
   if ret ~= 0 then goto OUT end
 
@@ -54,6 +65,7 @@ function git_show.lines(reponame, filename, commit_hash)
   content = ffi.string(data, len)
 
   ::OUT::
+  if index_ptr and index_ptr[0] then libgit2.cli.git_index_free(index_ptr[0]) end
   if blob_ptr and blob_ptr[0] then libgit2.cli.git_blob_free(blob_ptr[0]) end
   if entry_ptr and entry_ptr[0] then libgit2.cli.git_tree_entry_free(entry_ptr[0]) end
   if commit_tree_ptr and commit_tree_ptr[0] ~= nil then libgit2.cli.git_tree_free(commit_tree_ptr[0]) end
